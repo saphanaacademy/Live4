e3a0e4790eb5d427c7d26f104f4568cec8a3d1d1
SAP HANA Academy <hanaacademy@sap.com>
2015-05-29 09:35:32 -0700

    Update SALES_PER_LOCATION_PER_PRODUCT.sql

--- scripts/SALES_PER_LOCATION_PER_PRODUCT.sql
+++ scripts/SALES_PER_LOCATION_PER_PRODUCT.sql
@@ -4,7 +4,7 @@
 
 CREATE VIEW LIVE4.SALES_PER_LOCATION_PER_PRODUCT AS 
 SELECT
- ROW_NUMBER() OVER (ORDER BY MANDT, ERDAT, VBAP.WERKS, MATNR) as ROW_NUM,
+ ROW_NUMBER() OVER (ORDER BY MANDT, ERDAT, VBAP.WERKS, MATNR) AS ROW_NUM,
  TW.MANDT,
  VBAP.ERDAT,
  VBAP.WERKS,
@@ -12,20 +12,17 @@ SELECT
  VBAP.QUANTITY,
  VBAP.SALES_TOTAL, 
  DLA.AQI,
- CEILING(DLA.AQI/50) AS AQI_RANGE,
- MAP(CEILING(DLA.AQI/50),  1, 'Good', 2, 'Moderate', 3, 'Unhealthy for Sensitive Groups', 4, 'Unhealthy', 5, 'Very Unhealthy', 'Hazardous') LEVEL_OHC,
- MAP(CEILING(DLA.AQI/50),  1, 'Green', 2, 'Yellow', 3, 'Orange', 4, 'Red', 5, 'Purple', 'Maroon') COLOUR,
  DLT.TMP,
  DLH.HUM
 FROM 
  LIVE4.T001W_WS TW,
  (
  	SELECT
-	 TO_DATE(ERDAT,'yyyymmdd') as ERDAT,
+	 TO_DATE(ERDAT,'yyyymmdd') AS ERDAT,
 	 VBAP.WERKS,
 	 MATNR,
-	 SUM(KWMENG) as QUANTITY,
-	 TO_DECIMAL(SUM(KWMENG * NETWR), 10, 0) as SALES_TOTAL 
+	 SUM(KWMENG) AS QUANTITY,
+	 TO_DECIMAL(SUM(KWMENG * NETWR), 10, 0) AS SALES_TOTAL 
 	FROM 
 	 LIVE4.VBAP
 	GROUP BY 
@@ -37,21 +34,21 @@ FROM
 	SELECT 
 	 "aqi" AS AQI,
 	 "date_local" AS DATELOCAL,
-	 10000000 * "state_code" + 10000 * "county_code" + "site_num" AS SITENUM
+	 "state_code" * 10000000 + "county_code" * 10000 + "site_num" AS SITENUM
 	FROM LIVE4."DataLake_AQI"
  ) DLA,
  (
 	SELECT 
 	 "1st_max_value" AS TMP,
 	 "date_local" AS DATELOCAL,
-	 10000000 * "state_code" + 10000 * "county_code" + "site_num" AS SITENUM
+	 "state_code" * 10000000 + "county_code" * 10000 + "site_num" AS SITENUM
 	FROM LIVE4."DataLake_Temperature"
  ) DLT,
  (
 	SELECT 
 	 "1st_max_value" AS HUM,
 	 "date_local" AS DATELOCAL,
-	 10000000 * "state_code" + 10000 * "county_code" + "site_num" AS SITENUM
+	 "state_code" * 10000000 + "county_code" * 10000 + "site_num" AS SITENUM
 	FROM LIVE4."DataLake_Humidity"
  ) DLH
 
