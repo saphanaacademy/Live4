067eab5ea95876403104487558365c058a2b9931
SAP HANA Academy <hanaacademy@sap.com>
2015-06-01 10:03:39 -0700

    Update SALES_PER_LOCATION_PER_PRODUCT.sql

--- scripts/SALES_PER_LOCATION_PER_PRODUCT.sql
+++ scripts/SALES_PER_LOCATION_PER_PRODUCT.sql
@@ -2,7 +2,7 @@
 -- this view is used gather product sales from a store by date also showing the weather readings for that date
 -- this data will in turn be used with Predictive Analysis
 
-CREATE VIEW LIVE4.SALES_PER_LOCATION_PER_PRODUCT AS 
+CREATE VIEW "LIVE4"."SALES_PER_LOCATION_PER_PRODUCT" AS 
 SELECT
  ROW_NUMBER() OVER (ORDER BY MANDT, ERDAT, VBAP.WERKS, MATNR) AS ROW_NUM,
  TW.MANDT,
@@ -26,31 +26,28 @@ FROM
 	FROM 
 	 LIVE4.VBAP
 	GROUP BY 
-	 ERDAT, WERKS, MATNR 
+	 ERDAT, WERKS, MATNR  
  ) VBAP,
  (
 	SELECT 
 	 "aqi" AS AQI,
-	 "date_local" AS DATELOCAL,
+	 TO_DATE("date_local",'mm/dd/yyyy') AS DATELOCAL,
 	 "state_code" * 10000000 + "county_code" * 10000 + "site_num" AS SITENUM
-	FROM 
-	 LIVE4."DataLake_AQI"
+	FROM LIVE4."DataLake_AQI"
  ) DLA,
  (
 	SELECT 
 	 "1st_max_value" AS TMP,
-	 "date_local" AS DATELOCAL,
+	 TO_DATE("date_local",'mm/dd/yyyy') AS DATELOCAL,
 	 "state_code" * 10000000 + "county_code" * 10000 + "site_num" AS SITENUM
-	FROM 
-	 LIVE4."DataLake_Temperature"
+	FROM LIVE4."DataLake_Temperature"
  ) DLT,
  (
 	SELECT 
 	 "1st_max_value" AS HUM,
-	 "date_local" AS DATELOCAL,
+	 TO_DATE("date_local",'mm/dd/yyyy') AS DATELOCAL,
 	 "state_code" * 10000000 + "county_code" * 10000 + "site_num" AS SITENUM
-	FROM 
-	 LIVE4."DataLake_Humidity"
+	FROM LIVE4."DataLake_Humidity"
  ) DLH
 
 WHERE  	
@@ -61,8 +58,5 @@ WHERE
  DLT.SITENUM = TW.WS_TMP_SITENUM AND
  DLH.DATELOCAL = VBAP.ERDAT AND
  DLH.SITENUM = TW.WS_HUM_SITENUM 
- 
 ORDER BY
- TW.MANDT, VBAP.ERDAT, VBAP.WERKS, VBAP.MATNR WITH READ ONLY;
-
-SELECT TOP 1000 * FROM LIVE4.SALES_PER_LOCATION_PER_PRODUCT;
+ TW.MANDT, VBAP.ERDAT, VBAP.WERKS, VBAP.MATNR WITH READ ONLY
